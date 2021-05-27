ed55555f8f699caedb5608c6b10c8c8b4d2994c2
Victoria <vep002@gmail.com>
2021-05-27 17:33:49 -0500

    commit tree 7f221b1db75b4aadc2cf31d8458456662beaff2f

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,23 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      textInput: ""
+    };
+  }
+
+  changeTwitterMessage = (e) => {
+    let newMessage = e.target.value
+    this.setState({
+      textInput: newMessage
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.changeTwitterMessage} value={this.state.textInput} />
       </div>
     );
   }
